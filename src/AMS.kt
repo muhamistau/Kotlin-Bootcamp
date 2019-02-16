07437517ff1e564f6bd256e5f6a309be93d6e9de
muhamistau <islam.taufikurahman@gmail.com>
2019-02-16 11:34:46 +0700

    Required and Optional Argument(s)

--- src/AMS.kt
+++ src/AMS.kt
@@ -8,6 +8,13 @@ fun feedTheFish() {
     val day = randomDay()
     val food = fishFood(day)
     print("Today is $day and the fish eat $food")
+    shouldChangeWater(day, 20, 50) // Normal argument(s)
+    shouldChangeWater(day) // Required argument(s) only
+    shouldChangeWater(day, dirty = 50) // Required and optional argument(s) with argument name specified
+
+    if (shouldChangeWater(day)) {
+        println("Change the water today")
+    }
 }
 
 fun randomDay() : String {
@@ -24,4 +31,8 @@ fun fishFood(day : String) : String {
         "Sunday" -> "plankton"
         else -> "fasting"
     }
+}
+
+fun shouldChangeWater(day: String, temp: Int = 22, dirty: Int = 20) : Boolean {
+    return true
 }
