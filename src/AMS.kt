6abbbf2e12149db493669854bf49f58fb536ea7c
muhamistau <islam.taufikurahman@gmail.com>
2019-02-16 10:54:11 +0700

    Feed The Fish

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,19 +1,16 @@
 import java.util.*
 
 fun main(args: Array<String>) {
-    dayOfWeek(args)
+    feedTheFish()
 }
 
-fun dayOfWeek(args: Array<String>) {
-    print("What day is it today, ${args[0]}?\n")
-    when (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
-        1 -> print("Sunday")
-        2 -> print("Monday")
-        3 -> print("Tuesday")
-        4 -> print("Wednesday")
-        5 -> print("Thursday")
-        6 -> print("Friday")
-        7 -> print("Saturday")
-    }
+fun feedTheFish() {
+    val day = randomDay()
+    val food = "pellets"
+    print("Today is $day and the fish eat $food")
+}
 
+fun randomDay() : String {
+    val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(7)]
 }
