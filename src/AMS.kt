3772558da5f63cf215e501aafa25490990c6e3dc
muhamistau <islam.taufikurahman@gmail.com>
2019-02-16 16:23:08 +0700

    whatShouldIDo Program Practice

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,19 +1,24 @@
 import java.util.*
 
 fun main(args: Array<String>) {
+    println("Hello, ${args[0]}!")
     feedTheFish()
+
+    repeat(2) {
+        println("A fish is swimming")
+    }
 }
 
 fun feedTheFish() {
     val day = randomDay()
     val food = fishFood(day)
     print("Today is $day and the fish eat $food")
-    shouldChangeWater(day, 20, 50) // Normal argument(s)
-    shouldChangeWater(day) // Required argument(s) only
-    shouldChangeWater(day, dirty = 50) // Required and optional argument(s) with argument name specified
+//    shouldChangeWater(day, 20, 50) // Normal argument(s)
+//    shouldChangeWater(day) // Required argument(s) only
+//    shouldChangeWater(day, dirty = 50) // Required and optional argument(s) with argument name specified
 
     if (shouldChangeWater(day)) {
-        println("Change the water today")
+        println(", Change the water today")
     }
 }
 
@@ -25,7 +30,7 @@ fun randomDay() : String {
 fun fishFood(day : String) : String {
     return when (day) {
         "Monday" -> "flakes"
-        "Wednesday" -> "redworms"
+        "Wednesday" -> "red worms"
         "Thursday" -> "granules"
         "Friday" -> "mosquitoes"
         "Sunday" -> "plankton"
@@ -33,6 +38,21 @@ fun fishFood(day : String) : String {
     }
 }
 
-fun shouldChangeWater(day: String, temp: Int = 22, dirty: Int = 20) : Boolean {
-    return true
+fun getDirtySensorReading() = 20
+
+fun shouldChangeWater(day: String, temp: Int = 22, dirty: Int = getDirtySensorReading()) : Boolean {
+//    val isTooHot = temp > 30
+//    val isTooDirty = dirty > 30
+//    val isSunday = day == "Sunday"
+
+    fun isTooHot(temp: Int) = temp > 30
+    fun isTooDirty(dirty: Int) = dirty > 30
+    fun isSunday(day: String) = day == "Sunday"
+
+    return when {
+        isTooHot(temp) -> true
+        isTooDirty(dirty) -> true
+        isSunday(day) -> true
+        else -> false
+    }
 }
